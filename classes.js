1a4fde989f6b0e2b030a247bb7f8ffe353080b04
Steve Yoo <steve.yoo@hotmail.com>
2019-09-26 19:22:14 +1200

    update DOM exercise

--- classes.js
+++ classes.js
@@ -5,8 +5,10 @@ document.addEventListener('DOMContentLoaded', start)
 
 function start () {
   // The first example is done for you. Uncomment the line below and reload the browser.
-  // one()
-    
+  one()
+  two() 
+  three()
+  makeVisible()
   // Your turn! Create a new function called `two`, then call it from here.
 }
 
@@ -20,6 +22,20 @@ function one () {
 
 // CREATE FUNCTION two HERE
 
+function two () {
+  var two = document.getElementById('two')
+
+  two.classList.add('green')
+
+}
 // CREATE FUNCTION three HERE
 
+function three (){
+  var three = document.getElementById('three')
+
+  three.classList.add('pink')
+}
 // CREATE FUNCTION makeVisible HERE
+function makeVisible () {
+
+}
